465718e0dfb2853b4a571a6b16c4845c57c7215e
jypa56 <72617793+jypa56@users.noreply.github.com>
2020-11-11 12:15:17 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,121 +2,106 @@
 var express = require('express');
 var path = require('path');
 var https = require('https');
-var http = require('http');
-
-var PORT = process.env.PORT || 5000;
-
+var body = require('body-parser');
+var PORT  = process.env.PORT || 9000;
 var app = express();
-
+app.use(body());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    res.render('index', { fname: 'Prince', lName: 'Kung' });
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+//สร้าง route ขึ้นมา 1 ตัว โดยกำหนดให้ path คือ / หรือ index ของ host นั่นเอง
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-app.listen(PORT, function() {
+app.get('/login', function (req, res) {
+    res.render('index');
+});
+
+app.post('/login', function(req, res){
+   
+    var data = {
+       'username': req.body.apiUser,
+       'password': req.body.apiPass
+    }
+    console.log(data);
+    console.log(tuApi(data));
+    async function main() {
+        try {
+            var userInfo = await tuApi(data);
+            // tuApi(data).then((message) =>{
+            //     console.log('API success: ' + userInfo.resolve)
+            // }).catch((message) => {
+            //     console.log('API failed: '+ userInfo.reject)
+            // })
+            console.log(userInfo);
+            if(userInfo.status == true){
+                res.render('main', userInfo);
+            }else{
+                swal({
+                    title: "Invalid username or password",
+                     text: "Please enter username or password again.",
+                     type: "warning"
+                   })
+                
+            }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    main();
+
+    
+
+});
+
+//ทำการรันเซิฟเวอร์ตามพอร์ตที่กำหนดด้านบน
+app.listen(PORT, function () {
     console.log(`Listening on ${PORT}`)
 });
 
-app.get('/api', function(req, res) {
-    const queryParams = req.query;
-    console.log('param[1]:' + queryParams['user']);
-    res.send(queryParams);
-});
+// define tu api request
+async function tuApi(data){ 
+    return new Promise(function( resolve, reject) {
+        var options = {
+            'method': 'POST',
+            'hostname': 'restapi.tu.ac.th',
+            'path': '/api/v1/auth/Ad/verify',
+            'headers': {
+            'Content-Type': 'application/json',
+            'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
+            },
+        };
 
+       var userInfo;
 
-
-
-
-//var options = {
-//    'method': 'POST',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v1/auth/Ad/verify',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//};
-
-//var req = https.request(options, function (res) {
-//    var chunks = [];
-
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    });
-
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    });
-
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//});
-
-
-//var options = {
-//    'method': 'GET',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v2/std/fac/all',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//};
-
-//var req = https.request(options, function (res) {
-//    var chunks = [];
-
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    });
-
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    });
-
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//});
-
-//req.end();
-
-
-const options = {
-    hostname: 'restapi.tu.ac.th',
-    path: '/',
-    method: 'GET',
-    'headers': {
-        'Content-Type': 'application/json',
-
-    }
-};
-
-function dataCounter(inputs) {
-    let counter = 0;
-    for (const input of inputs) {
-        if (input.postId === 1) counter += 1;
-    }
-    return counter;
-};
-
-const req = http.request(options, function(response) {
-    response.setEncoding('utf8');
-    response.on('data', chunk => {
-        console.log('number of posts:' + dataCounter(chunk));
-    });
-
-    response.on('end', () => {
-        console.log('end of GET request');
-    });
-});
-
-req.on('error', e => {
-    console.log('Problem with request:', e.message);
-});
-req.end();
+        var request = https.request(options, function (res) {
+            var chunks = [];
+        
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+        
+            res.on("end", function (chunk) {
+                var body = Buffer.concat(chunks);
+                userInfo = JSON.parse(body);
+                resolve(userInfo);
+            });
+        
+            res.on("error", function (error) {
+                reject('error');
+                console.error(error);
+            });
+        });
+        
+        var postData =  `{\n\t\"UserName\":\"${data.username}\",\n\t\"PassWord\":\"${data.password}\"\n}`;
+        //console.log(postData);
+        request.write(postData);
+        request.end();
+        
+    })
+}
